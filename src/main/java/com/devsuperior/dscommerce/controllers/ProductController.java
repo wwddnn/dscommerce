889adc0c6a9b7b67caeca7204d09517423ad3840
Warley Dias <warleydias@gmail.com>
2023-09-12 22:42:29 -0300

    "GET product paged"

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,16 +1,15 @@
 package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -23,5 +22,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")/*casa esse id com o que esta embaixo, usamos entao o @PathVariable*/
     public ProductDTO findById(@PathVariable long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {/*Pageable poe a lista paginada qd exibe */
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service /*ja registra ele como componente, pode ser @Component ou @Service*/
@@ -17,10 +20,16 @@ public class ProductService {
 
     /*vai no banco de dados buscar o produto e converter pra DTO e retornar*/
     @Transactional(readOnly = true)/*como nao to salvando nada no banco, é 'somente leitura', isso é pra nao dar lock */
-    public ProductDTO findById(Long id) {/*retorna um DTO e nao um Product. O Service devolve DTO para o Controller*/
+    public ProductDTO findById(Long id) { /*retorna um DTO e nao um Product. O Service devolve DTO para o Controller*/
         Optional<Product> result = repository.findById(id);/*fui no banco de dados Repository e busquei o produto com esse id */
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);/*instancia um DTO aqui para ele transformar os dados que vieram do Repository */
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) { /*page é um tipo especial de lista*/
+        Page<Product> result = repository.findAll(pageable);/*busca todos os products*/
+        return result.map(x -> new ProductDTO(x));
     }
 }
